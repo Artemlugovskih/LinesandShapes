ad2385d637e1b4951e35578a93db7050fd9018d4
user07 <user07>
2022-11-14 17:13:14 +0300

    first commit

---
+++ app/src/main/java/com/example/linesandshapes/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.linesandshapes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun btnPlay(view: View) {}
+}
